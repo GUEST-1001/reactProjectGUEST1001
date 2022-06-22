9199e9302e008fe9c3e787f8ea71d4388a5e6b0d
GUEST-1001 <nawatgame@live.com>
2022-06-22 10:44:50 +0700

    Add stySheet styleSheet 220622 10:44

---
+++ components/stySheet.js
@@ -0,0 +1,23 @@
+import { StyleSheet } from 'react-native'
+
+const stySheet = StyleSheet.create({
+    Container: {
+      flex: 1,
+      padding : 24,
+      backgroundColor: "#eaeaea"
+    },
+    title: {
+      marginTop: 16,
+      paddingVertical: 8,
+      borderWidth: 4,
+      borderColor: "#20232a",
+      backgroundColor: "#61dafb",
+      borderRadius: 6,
+      color: "#20232a",
+      textAlign: "center",
+      fontSize: 30,
+      fontWeight: "bold"
+    }
+})
+
+export default stySheet;
