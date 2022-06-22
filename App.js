6a23a37093c61ca666bc320f0666712ba3acd983
GUEST-1001 <nawatgame@live.com>
2022-06-22 10:45:39 +0700

    Add stryleSheet from stySheet 220622 10:44

--- App.js
+++ App.js
@@ -1,13 +1,14 @@
 import { View, Text, StyleSheet } from "react-native";
 import React from "react";
-import Logo from "./components/Logo";
+// import Logo from "./components/Logo";
 import styles from "./components/styles"
+import stySheet from "./components/stySheet";
 
 const App = () => {
   return (
-    <View style={styles.container}>
-      <Text style={[styles.title,styles.warning]}>Hello React native</Text>
-      <Logo/>
+    <View style={stySheet.Container}>
+      <Text style={stySheet.title}>React native</Text>
+      {/* <Logo/> */}
     </View>
   );
 };
