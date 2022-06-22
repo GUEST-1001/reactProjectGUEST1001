a8f792abb29d7ae38c619ef4862fa9c014452ab8
GUEST-1001 <nawatgame@live.com>
2022-06-22 11:14:54 +0700

    change stySheet color 220622 11:14

--- components/stySheet.js
+++ components/stySheet.js
@@ -4,16 +4,16 @@ const stySheet = StyleSheet.create({
     Container: {
       flex: 1,
       padding : 24,
-      backgroundColor: "#eaeaea"
+      backgroundColor: "red"
     },
     title: {
       marginTop: 16,
       paddingVertical: 8,
       borderWidth: 4,
-      borderColor: "#20232a",
-      backgroundColor: "#61dafb",
+      borderColor: "yellow",
+      backgroundColor: "green",
       borderRadius: 6,
-      color: "#20232a",
+      color: "blue",
       textAlign: "center",
       fontSize: 30,
       fontWeight: "bold"
