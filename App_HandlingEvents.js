ca48620a70075da6589500b0f04b2c8cb61ddc8a
GUEST-1001 <nawatgame@live.com>
2022-06-29 10:37:56 +0700

    App.js >> App_HandlingEvents.js
    rename file App.js to App_HandlingEvents.js

---
+++ App_HandlingEvents.js
@@ -0,0 +1,28 @@
+import { StyleSheet, Text, View, Button } from 'react-native'
+import React from 'react'
+
+export default function App() {
+
+  const showData = () => {
+    alert("Hello")
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text>Button Example</Text>
+      <Button
+        title='click me'
+        color='purple'
+        onPress={showData}
+      />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex:1,
+    alignItems:'center',
+    justifyContent:'center'
+  }
+})
